18386f0ed01edbdaeb8a1c6e0dd2c9aeb5a26b44
Anush S <82928836+Anushsadesh@users.noreply.github.com>
2023-04-03 11:08:40 +0530

    Add files via upload

---
+++ zoho.py
@@ -0,0 +1,17 @@
+
+'''
+summer= 1 metre
+spring= should be doubled
+
+'''
+#summer=n+1
+import os
+os.system('cls')
+cycle=int(input())
+summer=1
+spring=1
+height=1
+for i in range(0,cycle+1):
+    height+=summer 
+    spr=spring*2
+print(height)
